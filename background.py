7c8a5e4a7f2dd55595afaa1b0eaf477f68a2e7a6
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-10-15 23:14:45 +0900

    파일 정리 및 클래스 사용
    클래스 사용

--- background.py
+++ background.py
@@ -1,30 +1,61 @@
 from pico2d import *
 
 open_canvas(1000, 500)
-image1 = load_image('land0001_tm001_bg.png')
-image2 = load_image('land0001_tm001_bg.png')
-character = load_image('brave_cookie_running.png')
-character_slide = load_image('brave_cookie_slide.png')
 
-image1Width = 500
-image2Width = 1500
-frame = 0
-running = True
+
+class Cookie:
+    def __init__(self):
+        self.Width, self.Height = 80, 100
+        self.x, self.y = 200, 90
+        self.rframe, self.sframe = 0, 0
+        self.rimage = load_image('brave_cookie_run.png')
+        self.simage = load_image('brave_cookie_slide.png')
+
+    def update(self):
+        self.rframe = (self.rframe + 1) % 4
+        self.sframe = (self.sframe + 1) % 2
+
+    def draw(self):
+        if dir == 0:
+            self.rimage.clip_draw(self.rframe*80, 0, self.Width, self.Height, self.x, self.y)
+        elif dir == 1:
+            self.simage.clip_draw(self.sframe*80, 0, self.Width, self.Height, self.x, self.y)
+
+
+class Land1:
+    def __init__(self):
+        self.x1, self.x2, self.y = 500, 1500, 250
+        self.Width, self.Height = 1000, 500
+        self.frame = 0
+        self.image1 = load_image('land1_stage1_bg.png')
+        self.image2 = load_image('land1_stage1_bg.png')
+
+    def update(self):
+        self.x1 -= 2
+        self.x2 -= 2
+
+    def draw(self):
+        self.image1.clip_draw(0, 0, self.Width, self.Height, self.x1, self.y)
+        self.image2.clip_draw(0, 0, self.Width, self.Height, self.x2, self.y)
+
+
+cookie = Cookie()
+land1 = Land1()
 dir = 0
+running = True
 
 
 def handle_events():
     global running
     global dir
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_DOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+            elif event.key == SDLK_DOWN:
                 dir += 1
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_DOWN:
@@ -32,23 +63,19 @@ def handle_events():
 
 
 while running:
-    clear_canvas()
-    image1.clip_draw(0, 0, 1000, 500, image1Width, 250)
-    image2.clip_draw(0, 0, 1000, 500, image2Width, 250)
-    if dir == 0:
-        character.clip_draw(frame * 80, 0, 80, 100, 100, 90)
-    elif dir == 1:
-        character_slide.clip_draw(frame * 80, 0, 80, 100, 100, 90)
-    if image1Width == -500:
-        image1Width = 500
-    if image2Width == 500:
-        image2Width = 1500
-    frame = (frame + 1) % 4
-    update_canvas()
-    image1Width -= 2
-    image2Width -= 2
+    handle_events()
 
-    delay(0.01)
+    land1.update()
+    cookie.update()
+
+    clear_canvas()
+
+    land1.draw()
+    cookie.draw()
+
+    update_canvas()
+
+    delay(0.05)
     get_events()
 
 close_canvas()
