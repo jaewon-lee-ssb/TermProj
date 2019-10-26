7b806875433c5662646a21da6a7d499ca39ec3f4
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-10-26 21:16:15 +0900

    슬라이드 재구현 및 프레임 워크 업로드

--- main_state.py
+++ main_state.py
@@ -2,24 +2,26 @@ from pico2d import *
 
 open_canvas(1000, 500)
 
+direction = 4
+
 
 class Cookie:
     def __init__(self):
         self.Width, self.Height = 80, 100
         self.x, self.y = 200, 90
-        self.rframe, self.sframe = 0, 0
-        self.rimage = load_image('brave_cookie_run.png')
-        self.simage = load_image('brave_cookie_slide.png')
+        self.frame = 0
+        self.image1 = load_image('brave_cookie_run.png')
+        self.image2 = load_image('brave_cookie_slide.png')
 
     def update(self):
-        self.rframe = (self.rframe + 1) % 4
-        self.sframe = (self.sframe + 1) % 2
+        delay(0.05)
+        self.frame = (self.frame + 1) % direction
 
     def draw(self):
-        if dir == 0:
-            self.rimage.clip_draw(self.rframe*80, 0, self.Width, self.Height, self.x, self.y)
-        elif dir == 1:
-            self.simage.clip_draw(self.sframe*80, 0, self.Width, self.Height, self.x, self.y)
+        if direction == 4:
+            self.image1.clip_draw(self.frame*80, 0, self.Width, self.Height, self.x, self.y)
+        elif direction == 2:
+            self.image2.clip_draw(self.frame * 110, 0, self.Width + 30, self.Height, self.x, self.y - 20, 130, 90)
 
 
 class Land1:
@@ -41,13 +43,12 @@ class Land1:
 
 cookie = Cookie()
 land1 = Land1()
-dir = 0
 running = True
 
 
 def handle_events():
     global running
-    global dir
+    global direction
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -55,11 +56,11 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
-            elif event.key == SDLK_DOWN:
-                dir += 1
+            elif event.key == SDLK_s:
+                direction = 2
         elif event.type == SDL_KEYUP:
-            if event.key == SDLK_DOWN:
-                dir -= 1
+            if event.key == SDLK_s:
+                direction = 4
 
 
 while running:
@@ -74,8 +75,5 @@ while running:
     cookie.draw()
 
     update_canvas()
-
-    delay(0.05)
-    get_events()
 
 close_canvas()
