acfe18a79623210bad98f213d2041ba82622df64
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-11-21 17:00:00 +0900

    장애물 수정 및 2가지 추가 구현

--- obstacle.py
+++ obstacle.py
@@ -1,33 +1,53 @@
 from pico2d import *
-from Position import *
 
 
 class Obstacle:
-    obstacle_image = None
-    obstacle_list = list()
+    obstacle_image1 = None
+    obstacle_image2 = None
+    obstacle_image3 = None
 
     def __init__(self):
-        global obstacle_image
-        if self.obstacle_image is None:
-            self.obstacle_image = load_image('Land\\land1_stage1_ob.png')
-        # for i in range(100):
-        #     temp = Position()
-        #     temp.x = 50 + i * 100
-        #     temp.y = 320
-        #     self.obstacle_list.append(temp)
-        self.obstacle_list = ((600, 320), (1000, 320))
+        if self.obstacle_image1 is None:
+            self.obstacle_image1 = load_image('Land\\land1_stage1_ob.png')
+        if self.obstacle_image2 is None:
+            self.obstacle_image2 = load_image('Land\\land1_stage1_ob.png')
+        if self.obstacle_image3 is None:
+            self.obstacle_image3 = load_image('Land\\land1_stage1_ob.png')
+        self.obstacle_list1 = [[600, 320], [1000, 320], [5000, 320]]
+        self.obstacle_list2 = [[800, 100]]
+        self.obstacle_list3 = [[1400, 100]]
 
     def draw(self, cookie):
-        for pos in self.obstacle_list:
-            self.obstacle_image.clip_draw(0, 0, 85, 256, pos.x - cookie.x + 200, pos.y, 85, 400)
-            draw_rectangle(pos.x - cookie.x + 200 - 30, pos.y - 180, pos.x - cookie.x + 200 + 30, pos.y + 160)
-            if pos.x - cookie.x > 1000:
+        for pos in self.obstacle_list1:
+            self.obstacle_image1.clip_draw(0, 0, 85, 256, pos[0] - cookie.x + 200, pos[1], 85, 400)
+            draw_rectangle(pos[0] - cookie.x + 200 - 30, pos[1] - 180, pos[0] - cookie.x + 200 + 30, pos[1] + 160)
+            if pos[0] - cookie.x > 1000:
+                break
+
+        for pos in self.obstacle_list2:
+            self.obstacle_image2.clip_draw(265, 200, 35, 60, pos[0] - cookie.x + 200, pos[1], 35, 60)
+            draw_rectangle(pos[0] - cookie.x + 200 - 20, pos[1] - 20, pos[0] - cookie.x + 200 + 20, pos[1] + 20)
+            if pos[0] - cookie.x > 1000:
+                break
+
+        for pos in self.obstacle_list3:
+            self.obstacle_image3.clip_draw(265, 200, 35, 60, pos[0] - cookie.x + 200, pos[1], 35, 60)
+            draw_rectangle(pos[0] - cookie.x + 200 - 20, pos[1] - 20, pos[0] - cookie.x + 200 + 20, pos[1] + 20)
+            if pos[0] - cookie.x > 1000:
                 break
 
     def update(self, cookie):
-        for pos in self.obstacle_list:
-            if cookie.x - pos.x > 250:
-                self.obstacle_list.pop(0)
-            elif not cookie.isSlide and pos.y - 180 < cookie.y + 50\
-                    and (pos.x < cookie.x - 40 < pos.x + 40 or pos.x - 30 < cookie.x + 40 < pos.x + 30):
-                self.obstacle_list.remove(pos)
+        left, bottom, right, top = cookie.get_bb()
+        for pos in self.obstacle_list1:
+            if cookie.x - pos[0] > 250:
+                self.obstacle_list1.pop(0)
+            elif pos[1] - 180 < top and (pos[0] - cookie.x + 200 - 30 < left < pos[0] - cookie.x + 200 + 30
+                                         or pos[0] - cookie.x + 200 - 30 < right < pos[0] - cookie.x + 200 + 30):
+                self.obstacle_list1.remove(pos)
+
+        for pos in self.obstacle_list2:
+            if cookie.x - pos[0] > 250:
+                self.obstacle_list2.pop(0)
+            elif pos[1] < top and (pos[0] - cookie.x + 200 - 20 < left < pos[0] - cookie.x + 200 + 20
+                                    or pos[0] - cookie.x + 200 - 20 < right < pos[0] - cookie.x + 200 + 20):
+                self.obstacle_list2.remove(pos)
