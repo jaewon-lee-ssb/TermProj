f2e63d818f17207f3870e071c185108842382a35
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-11-22 02:24:15 +0900

    자동 젤리 설치

--- jelly.py
+++ jelly.py
@@ -2,6 +2,7 @@ from pico2d import *
 import position
 import main_state
 
+
 class Jelly:
     jelly_image = None
     jelly_list = list()
@@ -25,8 +26,19 @@ class Jelly:
         for pos in obstacle.obstacle_list3:
             for i in self.jelly_list:
                 if pos[0] == i.x:
-                    i.y += 200
+                    i.y += 150
 
+        for i in range(len(self.jelly_list)):
+            if self.jelly_list[i].y == 200:
+                if i - 1 > 0:
+                    self.jelly_list[i - 1].y += 50
+                if i + 1 < len(self.jelly_list):
+                    self.jelly_list[i + 1].y += 50
+            elif self.jelly_list[i].y == 250:
+                if i - 1 > 0:
+                    self.jelly_list[i - 1].y += 100
+                if i + 1 < len(self.jelly_list):
+                    self.jelly_list[i + 1].y += 100
         # self.jelly_list = [[400, 100], [450, 100], [500, 100], [550, 100], [600, 100], [650, 100], [700, 100],
         #                   [750, 100], [800, 100], [850, 100], [900, 100], [950, 100], [1000, 100], [1050, 100],
         #                   [1100, 100], [1130, 150], [1150, 200], [1170, 240], [1200, 270], [1240, 300], [1270, 270],
@@ -40,6 +52,15 @@ class Jelly:
         # 바닥에 긴 장애물
         # self.obstacle_list3 = [[1250, 135], [2250, 135], [3100, 135], [3400, 135], [3700, 135], [4300, 135],
         #                       [4900, 135]]
+
+    def jelly_up(self, i):
+        (i - 1).y += 50
+        (i + 1).y += 50
+
+    def aaaa(self):
+        jelly_iter = self.jelly_list.__iter__()
+
+
 
     def draw(self, cookie):
         for pos in self.jelly_list:
