5427b076decd5a1e9a55700b1c7a50bd4e6c5e73
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-11-24 17:26:29 +0900

    생명아이템 사운드

--- item.py
+++ item.py
@@ -8,17 +8,37 @@ FRAMES_PER_ACTION = 4
 
 
 class Item:
-    heart_item_image = None
+    hp_item_image = None
 
     def __init__(self):
-        if self.heart_item_image is None:
-            self.heart_item_image = load_image('Jelly\\Jelly.png')
+        if self.hp_item_image is None:
+            self.hp_item_image = load_image('Jelly\\Jelly.png')
+        self.hp_item_sound = load_wav('Sound\\Hp_item.ogg')
+        self.hp_item_sound.set_volume(50)
+        self.hp_item_list = [[5300, 200]]
         self.frame = 0
+        self.count = 0
 
     def update(self):
+        cookie = main_state.get_cookie()
+        left, bottom, right, top = cookie.get_bb()
+        health = main_state.get_health()
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
-        pass
+
+        for pos in self.hp_item_list:
+            if cookie.x - pos[0] > 250:
+                self.hp_item_list.pop(0)
+            elif pos[0] - cookie.x < 400 and (bottom < pos[1] - 30 < top or bottom < pos[1] + 30 < top) \
+                    and (left < pos[0] - cookie.x + 200 - 30 < right or left < pos[0] - cookie.x + 200 + 30 < right):
+                self.hp_item_list.remove(pos)
+                self.hp_item_sound.play(1)
+                health.health += 200
+                self.count += 1
 
     def draw(self):
         cookie = main_state.get_cookie()
-        self.heart_item_image.clip_draw(1474 + int(self.frame) * 146, 958, 144, 144, 500 - cookie.x + 200, 300, 100, 100)
+        for pos in self.hp_item_list:
+            self.hp_item_image.clip_draw(1474 + int(self.frame) * 146, 958, 144, 144, pos[0] - cookie.x + 200, pos[1], 100, 100)
+            draw_rectangle(pos[0] - cookie.x + 200 - 30, pos[1] - 30, pos[0] - cookie.x + 200 + 30, pos[1] + 30)
+            if pos[0] - cookie.x > 1000:
+                break
