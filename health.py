1e27ee73e06b171af067365a655e7a2f44c8cf38
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-11-24 17:26:18 +0900

    체력바 고정

--- health.py
+++ health.py
@@ -16,18 +16,21 @@ class Health:
         self.health = 400
         self.move1 = 0
         self.move2 = 0
+        self.move3 = 0
 
     def update(self):
         obstacle = main_state.get_obstacle()
+        item = main_state.get_item()
         self.health -= game_framework.frame_time * 4
         self.move1 += game_framework.frame_time * 2
         if obstacle.timer == 3:
             self.move2 += 25
+        self.move3 = item.count * 100
         pass
 
     def draw(self):
         obstacle = main_state.get_obstacle()
         self.health_image1.clip_draw(0, 10, int(self.health), 90,
-                                     (self.left_x + self.right_x) / 2 - int(self.move1) - int(self.move2) - 100,
+                                     int(self.health) - 50 + self.move1 + self.move2 - self.move3,
                                      450, int(self.health), 20)
         self.health_image2.clip_draw(1475, 960, 140, 140, 120, 450, 95, 100)
