54eca47e2822ece035ab318618270d861f958881
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-12-10 15:10:50 +0900

    3스테이지 배경 완성

--- Code/land.py
+++ Code/land.py
@@ -28,7 +28,7 @@ class Land:
             self.land3_3_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage3_bg.png')
         if self.land3_4_image is None:
             self.land3_4_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage3_bg.png')
-            
+
         self.land1_sound = load_wav('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Sound\\Land11.ogg')
         self.land1_sound.set_volume(10)
         self.land1_sound.repeat_play()
@@ -40,6 +40,11 @@ class Land:
         cookie = main_state.get_cookie()
         if cookie.x < 5400:
             self.land1_image.clip_draw(0, 0, 1000, 500, 500, 250)
-        else:
+        elif 5400 < cookie.x < 8000:
             self.land2_2_image.clip_draw(1100, 0, 600, 500, 300, 250)
             self.land2_1_image.clip_draw(0, 0, 1000, 500, 499, 250)
+        else:
+            self.land3_1_image.clip_draw(0, 740, 1100, 370, 500, 315)
+            self.land3_2_image.clip_draw(0, 425, 1100, 310, 500, 250)
+            self.land3_3_image.clip_draw(0, 190, 1100, 240, 500, 150)
+            self.land3_4_image.clip_draw(0, 0, 1100, 190, 500, 80)
