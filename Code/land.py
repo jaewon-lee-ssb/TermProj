670224dca30c212e98a6409320c4b7623d9dcc2b
jaewon-lee-ssb <jaewon990905@gmail.com>
2019-12-10 14:55:53 +0900

    3스테이지 배경

--- Code/land.py
+++ Code/land.py
@@ -6,14 +6,29 @@ class Land:
     land1_image = None
     land2_1_image = None
     land2_2_image = None
+    land3_1_image = None
+    land3_2_image = None
+    land3_3_image = None
+    land3_4_image = None
 
     def __init__(self):
         if self.land1_image is None:
             self.land1_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage1_bg.png')
+
         if self.land2_1_image is None:
             self.land2_1_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage2_bg.png')
         if self.land2_2_image is None:
             self.land2_2_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage2_bg.png')
+
+        if self.land3_1_image is None:
+            self.land3_1_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage3_bg.png')
+        if self.land3_2_image is None:
+            self.land3_2_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage3_bg.png')
+        if self.land3_3_image is None:
+            self.land3_3_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage3_bg.png')
+        if self.land3_4_image is None:
+            self.land3_4_image = load_image('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Land\\land1_stage3_bg.png')
+            
         self.land1_sound = load_wav('C:\\Users\\jaewo\\Desktop\\2DGP\\TermProj\\Sound\\Land11.ogg')
         self.land1_sound.set_volume(10)
         self.land1_sound.repeat_play()
